f1a1b1d8da48f3868b973ae5d71057f57248c69f
kiran ram <kram@lions.molloy.edu>
2020-03-14 22:45:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
+  
+  attr_accessor :total, :title, :price, :quantity
+  @@total = 0
+  @@items = []
+  
+  def self.items
+    @@items
+  end
+  
+  
+  def initialize
+    @total = 0 
+    @@items << self
+  end
+  
+  def self.total
+    @total
+    
+  end
+  
+  def add_item(title, price)
+    @total += price 
+  end
 
+end
+
+def void_last_transaction
+  
+end
